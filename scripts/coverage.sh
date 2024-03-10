50d9452ce0870b25e0a55a02ce6b191cf00d32a5
gruvw <gruvw.dev@gmail.com>
2024-03-10 18:25:18 +0100

    Works fast CI

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,16 +1,10 @@
+# Setup
 rm -rf ./coverage
 dlcov gen-refs
 
-# Flutter only tests
-# flutter test --coverage -r github test || exit 1
-
-# Flutter tests + Integration tests
-flutter test --coverage -r github --coverage-path=coverage/lcov.base.info test
-flutter emulators --launch Medium_Phone_API_34
-adb wait-for-device
-flutter test --coverage -r github --merge-coverage integration_test -d sdk
-adb emu kill
+# Flutter tests
+flutter test --coverage -r github test
 
 # Show coverage info
 genhtml coverage/lcov.info -o coverage/html
-dlcov --coverage=80 --include-untested-files=true --log=true
+dlcov -c 80
--- test/component/overview_test.dart
+++ test/component/overview_test.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:flutter_demo/main.dart";
 import "package:flutter_demo/views/routes/overview.dart";
 import "package:flutter_test/flutter_test.dart";
 import "package:hooks_riverpod/hooks_riverpod.dart";
@@ -14,5 +15,20 @@ void main() {
 
     expect(find.text("hey"), findsOneWidget);
     expect(find.textContaining(DateTime.now().year.toString()), findsOneWidget);
+  });
+
+  group("Full app testing", () {
+    testWidgets("Create a new Todo", (tester) async {
+      await tester.pumpWidget(const MyApp());
+
+      expect(find.text("hey"), findsOneWidget);
+
+      final fab = find.byKey(const Key("new"));
+
+      expect(fab, findsOneWidget);
+
+      await tester.tap(fab);
+      await tester.pumpAndSettle();
+    });
   });
 }
