3b416fb67fbd445958c3cfc172d0976710086d56
gruvw <gruvw.dev@gmail.com>
2024-03-10 17:40:37 +0100

    New try

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,6 +1,9 @@
 dlcov gen-refs
-flutter test --coverage -r github --coverage-path=coverage/lcov.base.info test
+flutter test --coverage -r github --coverage-path=coverage/lcov.base.info test || exit 1
 flutter emulators --cold --launch Medium_Phone_API_34
-flutter test --coverage -r github --merge-coverage integration_test -d sdk
+adb wait-for-device
+flutter test --coverage -r github --merge-coverage integration_test -d sdk || (adb emu kill && exit 1)
+adb emu kill
 dlcov --coverage=80 --include-untested-files=true --log=true
 genhtml coverage/lcov.info -o coverage/html
+dlcov -c=80
